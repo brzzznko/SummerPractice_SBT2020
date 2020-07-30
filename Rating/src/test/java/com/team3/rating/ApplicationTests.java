80811776ab546fd79a3b358dcaed73d952e34710
Max Borzenko <zzzking999@yandex.ru>
2020-07-30 14:36:17 +0700

    try to fix mongo for travis-ci

--- Rating/src/test/java/com/team3/rating/ApplicationTests.java
+++ Rating/src/test/java/com/team3/rating/ApplicationTests.java
@@ -1,7 +1,5 @@
 package com.team3.rating;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
