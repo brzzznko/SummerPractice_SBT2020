9a2e43363f664c013110fc6f3ff6e9e385e07202
Max Borzenko <zzzking999@yandex.ru>
2020-08-03 21:17:45 +0700

    Add filling database before testing

--- Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
+++ Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
@@ -1,6 +1,7 @@
 package com.team3.collections.Controllers;
 
 import com.team3.collections.Database.CollectionsDataOperator;
+import org.bson.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 @SpringBootTest
 public class ApiControllerTest {
@@ -21,6 +24,33 @@ public class ApiControllerTest {
     private CollectionsDataOperator collectionsDataOperator;
 
     private final RestTemplate restTemplate = new RestTemplate();
+
+    @BeforeTestClass
+    public void fillDatabase() {
+        collectionsDataOperator.insertJson(new Document("collection_id", "25")
+                .append("owner_id", 1)
+                .append("name", "Макароны")
+                .append("description", "Сравнение любимых макарон")
+                .append("posts", Arrays.asList(11, 12, 13))
+                .append("criterion", Arrays.asList("Вкус", "Цена"))
+        );
+
+        collectionsDataOperator.insertJson(new Document("collection_id", "1")
+                .append("owner_id", 2)
+                .append("name", "Яблоки")
+                .append("description", "Сравнение")
+                .append("posts", Arrays.asList(33, 66, 88))
+                .append("criterion", Arrays.asList("Вкус", "Цена"))
+        );
+
+        collectionsDataOperator.insertJson(new Document("collection_id", "17")
+                .append("owner_id", 3)
+                .append("name", "Платки")
+                .append("description", "Сравнение")
+                .append("posts", Arrays.asList(1100, 1200, 1300))
+                .append("criterion", Arrays.asList("Вкус", "Цена"))
+        );
+    }
 
 
     @Test
