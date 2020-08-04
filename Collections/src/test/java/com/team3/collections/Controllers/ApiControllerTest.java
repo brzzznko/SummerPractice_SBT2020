5a22eb30b341a43acdcb1bad61ada0bbc4362e74
Max Borzenko <zzzking999@yandex.ru>
2020-08-04 13:06:06 +0700

    Add tests for deleting post from collection API method

--- Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
+++ Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
@@ -18,7 +18,12 @@ import java.util.Arrays;
 @SpringBootTest
 public class ApiControllerTest {
     private final RestTemplate restTemplate = new RestTemplate();
+
     private static final String TEST_COLLECTION_ID = "1";
+    private static final Integer DELETING_TEST_POST_ID = 33;
+    private static final String GOOD_TOKEN = "1";
+    private static final String BAD_TOKEN = "very_bad";
+
 
     @BeforeAll
     public static void fillDatabase(@Autowired CollectionsDataOperator collectionsDataOperator) {
@@ -26,7 +31,7 @@ public class ApiControllerTest {
                 .append("owner_id", 2)
                 .append("name", "Яблоки")
                 .append("description", "Сравнение")
-                .append("posts", Arrays.asList(33, 66, 88))
+                .append("posts", Arrays.asList(DELETING_TEST_POST_ID, 66, 88))
                 .append("criterion", Arrays.asList("Вкус", "Цена"))
         );
     }
@@ -39,11 +44,8 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Successfully deleting a collection")
     public void deleteCollection() throws URISyntaxException {
-        // Good token
-        String token = "1";
-
         //Http request
-        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + token;
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + GOOD_TOKEN;
         URI uri = new URI(url);
         ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.DELETE, null, String.class);
 
@@ -54,11 +56,34 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Not enough rights to delete collection")
     public void notRightsToDeleteCollection() throws URISyntaxException {
-        // Bad token
-        String token = "sdfjka";
-
         // Uri for request
-        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + token;
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + BAD_TOKEN;
+        URI uri = new URI(url);
+
+        // Try to make request and check Http status code
+        Assertions.assertThrows(HttpClientErrorException.Forbidden.class,
+                () -> restTemplate.exchange(uri, HttpMethod.DELETE, null, String.class));
+    }
+
+    @Test
+    @DisplayName("Successfully deleting post from collection")
+    public void deletePostFromCollection() throws URISyntaxException {
+        //Http request
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID +
+                "/post/" + DELETING_TEST_POST_ID + "/token/" + GOOD_TOKEN;
+        URI uri = new URI(url);
+        ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.DELETE, null, String.class);
+
+        // Check Http status code
+        Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Not enough rights to delete post from collection")
+    public void notRightsToDeletePostFromCollection() throws URISyntaxException {
+        // Uri for request
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID +
+                "/post/" + DELETING_TEST_POST_ID + "/token/" + BAD_TOKEN;
         URI uri = new URI(url);
 
         // Try to make request and check Http status code
