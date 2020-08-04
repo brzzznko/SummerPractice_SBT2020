ac3d8eb36654ba4af14bd5e456e3f14e63559533
Max Borzenko <zzzking999@yandex.ru>
2020-08-04 12:55:23 +0700

    Change tests for deleting collection API method

--- Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
+++ Collections/src/test/java/com/team3/collections/Controllers/ApiControllerTest.java
@@ -2,10 +2,7 @@ package com.team3.collections.Controllers;
 
 import com.team3.collections.Database.CollectionsDataOperator;
 import org.bson.Document;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
@@ -21,10 +18,11 @@ import java.util.Arrays;
 @SpringBootTest
 public class ApiControllerTest {
     private final RestTemplate restTemplate = new RestTemplate();
+    private static final String TEST_COLLECTION_ID = "1";
 
     @BeforeAll
     public static void fillDatabase(@Autowired CollectionsDataOperator collectionsDataOperator) {
-        collectionsDataOperator.insertJson(new Document("collection_id", "1")
+        collectionsDataOperator.insertJson(new Document("collection_id", TEST_COLLECTION_ID)
                 .append("owner_id", 2)
                 .append("name", "Яблоки")
                 .append("description", "Сравнение")
@@ -33,16 +31,19 @@ public class ApiControllerTest {
         );
     }
 
+    @AfterAll
+    public static void clearDatabase(@Autowired CollectionsDataOperator collectionsDataOperator) {
+        collectionsDataOperator.deleteCollection(TEST_COLLECTION_ID);
+    }
 
     @Test
-    @DisplayName("Success deletion")
+    @DisplayName("Successfully deleting a collection")
     public void deleteCollection() throws URISyntaxException {
         // Good token
         String token = "1";
-        String collectionId = "1";
 
         //Http request
-        String url = "http://localhost:8081/collections/" + collectionId + "/token/" + token;
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + token;
         URI uri = new URI(url);
         ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.DELETE, null, String.class);
 
@@ -55,10 +56,9 @@ public class ApiControllerTest {
     public void notRightsToDeleteCollection() throws URISyntaxException {
         // Bad token
         String token = "sdfjka";
-        String collectionId = "1";
 
         // Uri for request
-        String url = "http://localhost:8081/collections/" + collectionId + "/token/" + token;
+        String url = "http://localhost:8081/collections/" + TEST_COLLECTION_ID + "/token/" + token;
         URI uri = new URI(url);
 
         // Try to make request and check Http status code
