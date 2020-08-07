862d6d715880141e91cfb07a656d9124e55099b3
SanyaNagel <student17let@mail.ru>
2020-08-07 13:25:12 +0700

    Fix API Collection

--- Collections/src/main/java/com/team3/collections/Controllers/ApiController.java
+++ Collections/src/main/java/com/team3/collections/Controllers/ApiController.java
@@ -19,8 +19,9 @@ public class ApiController {
 
     /**
      * Delete collection by ID, if you have sufficient rights.
+     *
      * @param collectionId ID of collection
-     * @param token access token of user session
+     * @param token        access token of user session
      * @return HttpStatus with message
      */
     @DeleteMapping("/{collectionID}/token/{token}")
@@ -31,8 +32,7 @@ public class ApiController {
 
         if (canDeleteCollection) {
             collectionsDataOperator.deleteCollection(collectionId);
-        }
-        else {
+        } else {
             return new ResponseEntity<>("Not enough rights", HttpStatus.UNAUTHORIZED);
         }
 
@@ -41,9 +41,10 @@ public class ApiController {
 
     /**
      * Delete post from collection, if you have sufficient rights.
+     *
      * @param collectionId id of collection
-     * @param postId id of post
-     * @param token access token of user session
+     * @param postId       id of post
+     * @param token        access token of user session
      * @return HttpStatus with message
      */
     @DeleteMapping("/{collectionID}/post/{postID}/token/{token}")
@@ -55,8 +56,7 @@ public class ApiController {
 
         if (canDeletePost) {
             collectionsDataOperator.deletePostFromCollection(collectionId, postId);
-        }
-        else {
+        } else {
             return new ResponseEntity<>("Not enough rights", HttpStatus.UNAUTHORIZED);
         }
 
@@ -71,8 +71,7 @@ public class ApiController {
 
         if (canDeletePost) {
             collectionsDataOperator.deletePostFromAllCollection(postId);
-        }
-        else {
+        } else {
             return new ResponseEntity<>("Not enough rights", HttpStatus.UNAUTHORIZED);
         }
 
@@ -83,7 +82,7 @@ public class ApiController {
     public ResponseEntity<Document> getPosts(@PathVariable("collectionID") String collectionId) {
         List<String> posts = collectionsDataOperator.getPosts(collectionId);
 
-        if(posts == null) {
+        if (posts == null) {
             return new ResponseEntity<>(new Document("response", "Collection not found"), HttpStatus.NOT_FOUND);
         }
 
@@ -91,65 +90,75 @@ public class ApiController {
     }
 
     @PostMapping("/")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> createNewCollection(@RequestBody Document bodyRequest){
+    public ResponseEntity<String> createNewCollection(@RequestBody Document bodyRequest) {
         String currentToken = bodyRequest.getString("token");
         if (true) {
             bodyRequest.remove("token");
-            final String id = java.util.UUID.randomUUID().toString();           //Generating an ID
-            bodyRequest.append("collection_id", id);
-            collectionsDataOperator.insertJson(bodyRequest);
+
+            final String idCollection = java.util.UUID.randomUUID().toString(); //Generating an ID
+            bodyRequest.append("collection_id", idCollection);
+
+            String idPost = bodyRequest.getString("first_post_id");
+            bodyRequest.remove("first_post_id");
+
+            collectionsDataOperator.insertJson(bodyRequest);        //Add collection
+            collectionsDataOperator.addPost(idCollection, idPost);  //Add post to collection
         } else {
             return new ResponseEntity<>("Not enough rigths", HttpStatus.UNAUTHORIZED);
         }
-        return new ResponseEntity<>("OK", HttpStatus.OK);
+        return new ResponseEntity<>("The collection is created", HttpStatus.CREATED);
     }
 
     @GetMapping("/{collectionID}/token/{token}")
     public ResponseEntity<Document> getCollectionData(@PathVariable("collectionID") String collectionID,
-                                                      @PathVariable("token") String token){
+                                                      @PathVariable("token") String token) {
         String currentToken = token;
         if (true) {
             Document doc = collectionsDataOperator.getCollection(collectionID);
-            if(doc == null){
-                return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }else{
+            if (doc == null) {
+                return new ResponseEntity<>(new Document("response", "Collection not found"), HttpStatus.NOT_FOUND);
+            } else {
                 return new ResponseEntity<>(doc, HttpStatus.OK);
             }
         } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(new Document("response", "Not enough rigths"), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @GetMapping("/users/{userID}/token/{token}")
     public ResponseEntity<Document> getCollectionsUser(@PathVariable("userID") Integer userID,
-                                                       @PathVariable("token") String token){
+                                                       @PathVariable("token") String token) {
         String currentToken = token;
         if (true) {
             Document doc = collectionsDataOperator.getListCollectionsUser(userID);
-            if(doc == null){
-                return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }else{
+            if (doc == null) {
+                return new ResponseEntity<>(
+                        new Document("response", "The user doesn't have any collections"),
+                        HttpStatus.NO_CONTENT);
+            } else {
                 return new ResponseEntity<>(doc, HttpStatus.OK);
             }
         } else {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(new Document("response", "Not enough rigths"), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @PutMapping("/")
-    public ResponseEntity<String> updateCollection(@RequestBody Document bodyRequest){
+    public ResponseEntity<String> updateCollection(@RequestBody Document bodyRequest) {
         String id = bodyRequest.getString("collection_id");
-        collectionsDataOperator.updateCollection(bodyRequest, id);
-        return new ResponseEntity<>("Successful collection update!", HttpStatus.OK);
+        if (collectionsDataOperator.updateCollection(bodyRequest, id) != true) {
+            return new ResponseEntity<>("You can't add a new criteria", HttpStatus.NOT_ACCEPTABLE);
+        } else {
+            return new ResponseEntity<>("Successful collection update!", HttpStatus.OK);
+        }
     }
 
     @PutMapping("/posts")
-    public ResponseEntity<String> addPostToCollection(@RequestBody Document bodyRequest){
+    public ResponseEntity<String> addPostToCollection(@RequestBody Document bodyRequest) {
         String currentToken = bodyRequest.getString("token");
         if (true) {
             String idCollection = bodyRequest.getString("collection_id");
-            if(idCollection == null){
+            if (idCollection == null) {
                 return new ResponseEntity<>("Not found collection ID", HttpStatus.NOT_FOUND);
             }
             collectionsDataOperator.addPost(idCollection, bodyRequest.getString("post_id"));
@@ -163,7 +172,7 @@ public class ApiController {
     @DeleteMapping("/posts/token/{token}")
     public ResponseEntity<String> deleteListPostsFromCollections(@RequestParam("postsList") List<String> postsList,
                                                                  @PathVariable("token") String token) {
-        for(String post : postsList) {
+        for (String post : postsList) {
             boolean canDeleteListPosts = token.equals("1");
             if (canDeleteListPosts) {
                 collectionsDataOperator.deletePostFromAllCollection(post);
